9564e5628f8f748ad3757b88f59dc488f8fa6a9b
Kobe <kobeneilson@gmail.com>
2023-09-29 14:58:22 -0700

    Added watchdog listener code
    Used the watchdog library to listen for changes to files in the designated file path

--- py-timer-script.py
+++ py-timer-script.py
@@ -1,1 +1,37 @@
+import os
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
+# Specify the directory you want to watch
+path_to_watch = "/path/to/your/directory"
+
+class MyHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        print(f"{event.src_path} was created")
+
+    def on_deleted(self, event):
+        print(f"{event.src_path} was deleted")
+
+    def on_modified(self, event):
+        print(f"{event.src_path} has been modified")
+
+    def on_moved(self, event):
+        print(f"moved {event.src_path} to {event.dest_path}")
+
+# Create an observer
+observer = Observer()
+
+# Attach the handler to the observer
+event_handler = MyHandler()
+observer.schedule(event_handler, path=path_to_watch, recursive=False)
+
+# Start the observer
+observer.start()
+
+try:
+    while True:
+        pass
+except KeyboardInterrupt:
+    observer.stop()
+
+observer.join()
