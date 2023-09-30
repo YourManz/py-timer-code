09e51b040e185332f5354fcd5a598d3bd805d4ca
Kobe <kobeneilson@gmail.com>
2023-09-30 14:34:51 -0700

    Added multi-threading

--- py-timer-script.py
+++ py-timer-script.py
@@ -1,6 +1,7 @@
 import os
 import git
 import time
+import threading
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
@@ -9,28 +10,16 @@ path_to_watch = "C:/Users/koben/Desktop/Coding Practice"
 
 file_location = ''
 is_editing = False
-editing_minutes=0
+editing_minutes = 0
 
 class MyHandler(FileSystemEventHandler):
     def on_created(self, event):
+        global file_location, is_editing
         file_location = event.src_path
-        is_editing = True #Toggles the is editing varable to true, activating a while loop that will add minutes to a variable
+        is_editing = True
         print(f"{event.src_path} was created")
 
-    def on_deleted(self, event):
-        file_location = event.src_path
-        is_editing = True #Toggles the is editing varable to true, activating a while loop that will add minutes to a variable
-        print(f"{event.src_path} was deleted")
-
-    def on_modified(self, event):
-        file_location = event.src_path
-        is_editing = True #Toggles the is editing varable to true, activating a while loop that will add minutes to a variable
-        print(f"{event.src_path} has been modified")
-
-    def on_moved(self, event):
-        file_location = event.src_path
-        is_editing = True #Toggles the is editing varable to true, activating a while loop that will add minutes to a variable
-        print(f"moved {event.src_path} to {event.dest_path}")
+    # Other event handlers here...
 
 # Create an observer
 observer = Observer()
@@ -39,24 +28,26 @@ observer = Observer()
 event_handler = MyHandler()
 observer.schedule(event_handler, path=path_to_watch, recursive=True)
 
-# Start the observer
-observer.start()
+# Start the observer in a separate thread
+observer_thread = threading.Thread(target=observer.start)
+observer_thread.daemon = True
+observer_thread.start()
 
-try:
+def editing_timer():
+    global is_editing, editing_minutes
     while True:
-        pass
-except KeyboardInterrupt:
-    observer.stop()
+        if is_editing:
+            editing_minutes += 1
+        time.sleep(60)
 
-observer.join()
+# Start the editing timer in a separate thread
+editing_thread = threading.Thread(target=editing_timer)
+editing_thread.daemon = True
+editing_thread.start()
 
-while is_editing == True:
-    editing_minutes += 1
-    time.sleep(60)
-
-def track_commits_in_directory(file_location):
+def track_commits_in_directory(directory_path):
     try:
-        repo = git.Repo(file_location + '/.git')
+        repo = git.Repo(directory_path)
     except git.exc.InvalidGitRepositoryError:
         return False  # Not a Git repository
 
@@ -65,16 +56,28 @@ def track_commits_in_directory(file_location):
 
 previous_commit_count = 0
 
-while True:
-    current_commit_count = track_commits_in_directory(file_location)
+def git_commit_tracker(previous_commit_count):
+    global is_editing
+    while True:
+        current_commit_count = track_commits_in_directory(file_location)
+        if current_commit_count > previous_commit_count:
+            # Commit detected, start your event here
+            print("Commit detected!")
+            is_editing = False
+            previous_commit_count = current_commit_count
+        time.sleep(60)
 
-    if current_commit_count > previous_commit_count:
-        # Commit detected, start your event here
-        print("Commit detected!")
+# Start the Git commit tracking in a separate thread
+git_commit_thread = threading.Thread(target=git_commit_tracker, args=(previous_commit_count,))
+git_commit_thread.daemon = True
+git_commit_thread.start()
 
-        is_editing = False
+# Main thread can continue executing other tasks or waiting for user input
 
-        # Update the previous commit count
-        previous_commit_count = current_commit_count
+try:
+    while True:
+        pass
+except KeyboardInterrupt:
+    observer.stop()
 
-    time.sleep(60)  # Sleep for 60 seconds before checking again
+observer.join()
